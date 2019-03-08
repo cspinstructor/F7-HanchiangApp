6c2bf10110fa9269e5a8b2ed77696493fe2abc4b
paulchin <paulchin20@yahoo.com>
2019-03-08 15:09:40 +0800

    fixed wp-api

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -61,21 +61,15 @@ $$('.panel-left').on('panel:open', function() {
 
 //--- Hanchiang News ---
 function getNews() {
-  alert('getnews()');
   var newsContent = '';
   //const apiRoot = 'https://hjuapp.site/wp-json';
   const apiRoot = 'http://www.hanchiangnews.com/en/wp-json';
   var wp = new WPAPI({ endpoint: apiRoot });
-  wp.posts()
-    .perPage(3)
-    .then(posts => {
-      posts.forEach(function(post) {
-        console.log(post.content.rendered);
-        newsContent += post.content.rendered;
-      });
-      $$('#id-news-content').html(newsContent);
-    })
-    .catch(err => {
-      console.log('Error: ' + err);
+  wp.posts().then(function(posts) {
+    posts.forEach(function(post) {
+      console.log(post.content.rendered);
+      newsContent += post.content.rendered;
     });
+    $$('#id-news-content').html(newsContent);
+  });
 }
